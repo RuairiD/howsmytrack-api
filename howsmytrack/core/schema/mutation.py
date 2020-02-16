dc6cb741baf2517c36a1c871a4c93ce9c99b316e
Ruairi Dorrity <ruairidorrity@gmail.com>
2020-02-16 10:54:38 -0800

    Added mutation for deleting a request if it's ungrouped.

--- howsmytrack/core/schema/mutation.py
+++ howsmytrack/core/schema/mutation.py
@@ -304,6 +304,59 @@ class EditFeedbackRequest(graphene.Mutation):
         )
 
 
+class DeleteFeedbackRequest(graphene.Mutation):
+
+    class Arguments:
+        feedback_request_id = graphene.Int(required=True)
+
+    success = graphene.Boolean()
+    error = graphene.String()
+
+    def __eq__(self, other):
+        return all([
+            self.success == other.success,
+            self.error == other.error,
+        ])
+
+    def mutate(self, info, feedback_request_id):
+        user = info.context.user
+        if user.is_anonymous:
+            return DeleteFeedbackRequest(
+                success=False,
+                error='Not logged in.',
+            )
+
+        feedback_groups_user = FeedbackGroupsUser.objects.filter(
+            user=user,
+        ).first()
+    
+        # Reject the deletion if the user does not own the request (or if it doesn't exist)
+        feedback_request = FeedbackRequest.objects.filter(
+            user=feedback_groups_user,
+            id=feedback_request_id,
+        ).first()
+        if not feedback_request:
+            return DeleteFeedbackRequest(
+                success=False,
+                error='You are not the owner of this feedback request.',
+            )
+
+        # Reject the deletion if the request has already been assigned to a group.
+        if feedback_request.feedback_group:
+            return DeleteFeedbackRequest(
+                success=False,
+                error='This request has already been assigned to a feedback group and cannot be edited.',
+            )
+        
+        # No problems; delete it.
+        feedback_request.delete()
+
+        return DeleteFeedbackRequest(
+            success=True,
+            error=None,
+        )
+
+
 class SubmitFeedbackResponse(graphene.Mutation):
 
     class Arguments:
@@ -397,6 +450,7 @@ class RateFeedbackResponse(graphene.Mutation):
 class Mutation(graphene.ObjectType):
     register_user = RegisterUser.Field()
     create_feedback_request = CreateFeedbackRequest.Field()
+    delete_feedback_request = DeleteFeedbackRequest.Field()
     edit_feedback_request = EditFeedbackRequest.Field()
     submit_feedback_response = SubmitFeedbackResponse.Field()
     rate_feedback_response = RateFeedbackResponse.Field()
--- howsmytrack/core/tests/schema/test_mutation.py
+++ howsmytrack/core/tests/schema/test_mutation.py
@@ -11,6 +11,7 @@ from howsmytrack.core.models import FeedbackRequest
 from howsmytrack.core.models import FeedbackResponse
 from howsmytrack.core.models import MediaTypeChoice
 from howsmytrack.core.schema.mutation import CreateFeedbackRequest
+from howsmytrack.core.schema.mutation import DeleteFeedbackRequest
 from howsmytrack.core.schema.mutation import EditFeedbackRequest
 from howsmytrack.core.schema.mutation import INVALID_MEDIA_URL_MESSAGE
 from howsmytrack.core.schema.mutation import INVALID_PASSWORD_MESSAGE
@@ -696,6 +697,112 @@ class EditFeedbackRequestTest(TestCase):
                 email_when_grouped=True,
             ).count(),
             1,
+        )
+
+
+class DeleteFeedbackRequestTest(TestCase):
+    def setUp(self):
+        self.user = FeedbackGroupsUser.create(
+            email='graham@brightonandhovealbion.com',
+            password='password',
+        )
+        self.user.save()
+        self.other_user = FeedbackGroupsUser.create(
+            email='lewis@brightonandhovealbion.com',
+            password='password',
+        )
+        self.other_user.save()
+
+        self.existing_request = FeedbackRequest(
+            user=self.user,
+            media_url='https://soundcloud.com/ruairidx/grey',
+            media_type=MediaTypeChoice.SOUNDCLOUD.name,
+            feedback_prompt='feedback_prompt',
+            email_when_grouped=True,
+        )
+        self.existing_request.save()
+
+    def test_logged_out(self):
+        info = Mock()
+        result = schema.get_mutation_type().fields['deleteFeedbackRequest'].resolver(
+            self=Mock(),
+            info=info,
+            feedback_request_id=self.existing_request.id,
+        )
+
+        self.assertEqual(result, DeleteFeedbackRequest(
+            success=False,
+            error='Not logged in.',
+        ))
+
+        self.assertEqual(
+            FeedbackRequest.objects.count(),
+            1,
+        )
+
+    def test_assigned_request(self):
+        feedback_group = FeedbackGroup(name='name')
+        feedback_group.save()
+        self.existing_request.feedback_group = feedback_group
+        self.existing_request.save()
+
+        info = Mock()
+        info.context = Mock()
+        info.context.user = self.user.user
+        result = schema.get_mutation_type().fields['deleteFeedbackRequest'].resolver(
+            self=Mock(),
+            info=info,
+            feedback_request_id=self.existing_request.id,
+        )
+
+        self.assertEqual(result, DeleteFeedbackRequest(
+            success=False,
+            error='This request has already been assigned to a feedback group and cannot be edited.',
+        ))
+
+        self.assertEqual(
+            FeedbackRequest.objects.count(),
+            1,
+        )
+
+    def test_unauthorised(self):
+        info = Mock()
+        info.context = Mock()
+        info.context.user = self.other_user.user
+        result = schema.get_mutation_type().fields['deleteFeedbackRequest'].resolver(
+            self=Mock(),
+            info=info,
+            feedback_request_id=self.existing_request.id,
+        )
+
+        self.assertEqual(result, DeleteFeedbackRequest(
+            success=False,
+            error='You are not the owner of this feedback request.',
+        ))
+
+        self.assertEqual(
+            FeedbackRequest.objects.count(),
+            1,
+        )
+
+    def test_ok(self):
+        info = Mock()
+        info.context = Mock()
+        info.context.user = self.user.user
+        result = schema.get_mutation_type().fields['deleteFeedbackRequest'].resolver(
+            self=Mock(),
+            info=info,
+            feedback_request_id=self.existing_request.id,
+        )
+
+        self.assertEqual(result, CreateFeedbackRequest(
+            success=True,
+            error=None,
+        ))
+
+        self.assertEqual(
+            FeedbackRequest.objects.count(),
+            0,
         )
 
 
