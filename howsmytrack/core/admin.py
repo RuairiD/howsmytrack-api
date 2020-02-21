c07d7d6f818efc0e863d2f4293ff2570a653cb63
Ruairi Dorrity <ruairidorrity@gmail.com>
2020-02-21 11:50:09 -0800

    Search and field columns for admin.

--- howsmytrack/core/admin.py
+++ howsmytrack/core/admin.py
@@ -4,18 +4,49 @@ from howsmytrack.core.models import FeedbackGroup
 from howsmytrack.core.models import FeedbackRequest
 from howsmytrack.core.models import FeedbackResponse
 
+
 class FeedbackRequestInline(admin.TabularInline):
     model = FeedbackRequest
     max_num = 4
     extra = 0
 
+
 class FeedbackGroupAdmin(admin.ModelAdmin):
     fields = ('name', )
+    search_fields = ['name']
     inlines = [
         FeedbackRequestInline
     ]
 
-admin.site.register(FeedbackGroupsUser)
+
+class FeedbackGroupsUserAdmin(admin.ModelAdmin):
+    search_fields = ['user__username']
+    list_display = (
+        'user',
+        'rating',
+    )
+    inlines = [
+        FeedbackRequestInline
+    ]
+
+
+class FeedbackResponseAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'feedback_request',
+        'feedback',
+        'submitted',
+        'rating',
+    )
+    search_fields = ['user__user__username']
+
+
+class FeedbackRequestAdmin(admin.ModelAdmin):
+    list_display = ('user','media_url','media_type','time_created','feedback_group')
+    search_fields = ['user__user__username']
+
+
+admin.site.register(FeedbackGroupsUser, FeedbackGroupsUserAdmin)
 admin.site.register(FeedbackGroup, FeedbackGroupAdmin)
-admin.site.register(FeedbackRequest)
-admin.site.register(FeedbackResponse)
+admin.site.register(FeedbackRequest, FeedbackRequestAdmin)
+admin.site.register(FeedbackResponse, FeedbackResponseAdmin)
